dd25d7fad83f12fca8d663455b9a9e24ee6bf328
lhr <lhr_freeany@163.com>
2025-07-09 14:30:55 +0800

    feat: init project

---
+++ src/renderer.tsx
@@ -0,0 +1,39 @@
+/**
+ * This file will automatically be loaded by vite and run in the "renderer" context.
+ * To learn more about the differences between the "main" and the "renderer" context in
+ * Electron, visit:
+ *
+ * https://electronjs.org/docs/tutorial/process-model
+ *
+ * By default, Node.js integration in this file is disabled. When enabling Node.js integration
+ * in a renderer process, please be aware of potential security implications. You can read
+ * more about security risks here:
+ *
+ * https://electronjs.org/docs/tutorial/security
+ */
+
+import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App';
+
+console.log('👋 This message is being logged by "renderer.tsx", included via Vite');
+
+// 创建根元素
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  const root = document.createElement('div');
+  root.id = 'root';
+  document.body.appendChild(root);
+  ReactDOM.createRoot(root).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
